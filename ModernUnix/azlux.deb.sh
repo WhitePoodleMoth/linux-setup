e75eeda48c3b4c7bec3e970e952bb42905e57b91
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2023-08-11 16:40:43 -0300

    Update azlux.deb.sh

--- ModernUnix/azlux.deb.sh
+++ ModernUnix/azlux.deb.sh
@@ -1,2 +1,14 @@
-echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ stable main" | sudo tee /etc/apt/sources.list.d/azlux.list
-sudo wget -O /usr/share/keyrings/azlux-archive-keyring.gpg  https://azlux.fr/repo.gpg
+#!/bin/bash
+
+# Verificar se a entrada já existe no arquivo sources.list.d/azlux.list
+if ! grep -q "azlux-archive-keyring.gpg" /etc/apt/sources.list.d/azlux.list; then
+    echo "Adicionando entrada ao arquivo sources.list.d/azlux.list..."
+    echo "deb [signed-by=/usr/share/keyrings/azlux-archive-keyring.gpg] http://packages.azlux.fr/debian/ stable main" | sudo tee -a /etc/apt/sources.list.d/azlux.list
+fi
+
+# Verificar se o arquivo azlux-archive-keyring.gpg já existe
+if [ ! -f "/usr/share/keyrings/azlux-archive-keyring.gpg" ]; then
+    echo "Baixando o arquivo azlux-archive-keyring.gpg..."
+    sudo wget -O /usr/share/keyrings/azlux-archive-keyring.gpg https://azlux.fr/repo.gpg
+fi
+
