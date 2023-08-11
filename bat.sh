3287b6c3f666e398fad6cdfb337b6d4af61cbdd2
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2023-08-11 17:00:37 -0300

    Update bat.sh

--- bat.sh
+++ bat.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-apt-get install bat
-ln -s /usr/bin/batcat /usr/local/bin/bat
+apt install bat
+
+if [ ! -L "/usr/local/bin/bat" ]; then
+    echo "Creating symbolic link..."
+    sudo ln -s /usr/bin/batcat /usr/local/bin/bat
+else
+    echo "Symbolic link already exists."
+fi
