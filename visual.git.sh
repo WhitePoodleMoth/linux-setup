9fe3d3c5c2c427886e1c5ace5d38472fd5e5770e
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2023-08-11 16:43:40 -0300

    Create visual.git.sh

---
+++ visual.git.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! grep -q "parse_git_branch() {" ~/.bashrc; then
+  echo 'parse_git_branch() {' >> ~/.bashrc
+  echo '  git branch 2>/dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/ (\1)/"' >> ~/.bashrc
+  echo '}' >> ~/.bashrc
+fi
+
+if ! grep -q "export PS1=.*parse_git_branch.*" ~/.bashrc; then
+  echo 'export PS1="\[\033[38;5;10m\]\u@\h\[\033[00m\]:\[\033[38;5;14m\]\w\[\033[31m\]$(parse_git_branch)\[\033[00m\] $ "' >> ~/.bashrc
+fi
+
